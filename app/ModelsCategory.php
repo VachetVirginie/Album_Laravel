0238b2dd0c2aaef716bbcbbcc92e453094bd7bc9
Vachet Virginie <vachetvirginie@gmail.com>
2018-04-15 19:01:08 +0200

    Les données

---
+++ app/ModelsCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Image;
+use App\Events\CategorySaving;
+
+class Category extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug',
+    ];
+
+    /**
+     * Get the images.
+     */
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+}
