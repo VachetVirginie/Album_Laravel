7002f2f1be241243a2ec08a365ad35bdfaae0d65
Vachet Virginie <vachetvirginie@gmail.com>
2018-04-22 18:54:31 +0200

    Gestion des categories ok !!!!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Pagination\AbstractPaginator;
+use App\Models\Category;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::if('admin', function () {
+            return auth()->check() && auth()->user()->role === 'admin';
+        });
+        if(request()->server("SCRIPT_NAME") !== 'artisan') {
+            view ()->share ('categories', Category::all ());
+        }
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,17 @@
 */
 
 Auth::routes();
+
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::middleware('admin')->group(function () {
+    Route::resource ('category', 'CategoryController', [
+        'except' => 'show'
+    ]);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::resource('image', 'ImageController', [
+        'only' => ['create', 'store', 'destroy']
+    ]);
+});
